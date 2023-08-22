b650772db2efe95f03430acc41a1b5aecf6a7d20
GyoungHwan-Hong <hongkh5218@gmail.com>
2023-08-22 14:23:32 -0700

    Write basic information.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          안녕하세요. 
         </p>
+        <p>
+          10월 15일 일요일 12시
+        </p>
+        <p>
+          서울특별시 용산구 소월로 302
+        </p>
+        <p>
+          앨리스몽드
+        </p>
+        <h1>
+          홍경환, 이현경
+        </h1>
+        <h1>
+          결혼합니다.
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
