742532b745f021f9d641ceab2ccacc5f2aea3746
GyoungHwan-Hong <hongkh5218@gmail.com>
2023-08-22 20:55:02 -0700

    Add basic informations and layouts

--- src/App.js
+++ src/App.js
@@ -23,25 +23,22 @@ function App() {
     <div className="App">
         <p>
           안녕하세요. 
+          저희가 결혼합니다.
         </p>
         <p>
-          10월 15일 일요일 12시
+          2023년 10월 15일 일요일 12시
         </p>
         <p>
-          서울특별시 용산구 소월로 302
-        </p>
-        <p>
-          앨리스몽드
+          서울특별시 용산구 소월로 302 앨리스몽드
         </p>
         <h1>
-          홍경환, 이현경
+          홍석길 심미경의 장남 홍경환
         </h1>
         <h1>
-          결혼합니다.
+          이종호 엄은희의 장녀 이현경
         </h1>
         <PhotoAlbum layout="rows" photos={firstRowPhotos} />
         <PhotoAlbum layout="rows" photos={secondRowPhotos} />
-
         <p>
           오시는 길
         </p>
@@ -49,6 +46,20 @@ function App() {
         <p>
           서울 특별시 용산구 소월로 302
         </p>
+
+        <p>
+          신랑에게 연락하기
+        </p>
+        <p>
+          신부에게 연락하기
+        </p>
+
+        <p>신랑 신부에게 마음 전하기</p>
+        <p>신랑측 마음</p>
+        <p>신부측 마음</p>
+
+
+        <p>공유하기</p>
     </div>
   );
 }
